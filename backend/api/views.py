1550e3ced639ba32f7059bf0c1c91e28d06d2b40
Yulian Tolkachov <56gymnasia@gmail.com>
2025-10-02 18:27:04 +0300

    fix:error handling

--- backend/api/views.py
+++ backend/api/views.py
@@ -15,7 +15,8 @@ def addEvent(request):
 	serializer = EventSerializer(data=request.data)
 	if serializer.is_valid():
 		serializer.save()
-	return Response(serializer.data)
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["GET", "PUT", "PATCH", "DELETE"])
 def editEvent(request, event_id):
--- backend/base/models.py
+++ backend/base/models.py
@@ -4,16 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Event(models.Model):
-    title = models.CharField(max_length=200, blank=True)
-    game_setting = models.CharField(max_length=200, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    date_start = models.DateTimeField(blank=True, null=True)
-    date_end = models.DateTimeField(blank=True, null=True)
-    online = models.BooleanField(default=True)
-    location = models.CharField(max_length=200, default="cafe/Discord server", blank=True, null=True)
-    # organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="organized_events")
-    created = models.DateTimeField(auto_now_add=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True)
+	title = models.CharField(max_length=200, blank=True)
+	game_setting = models.CharField(max_length=200, blank=True, null=True)
+	description = models.TextField(blank=True, null=True)
+	date_start = models.DateTimeField(blank=True, null=True)
+	date_end = models.DateTimeField(blank=True, null=True)
+	online = models.BooleanField(default=True)
+	location = models.CharField(max_length=200, default="cafe/Discord server", blank=True, null=True)
+	# organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="organized_events")
+	created = models.DateTimeField(auto_now_add=True, blank=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.title} ({self.date_start.date()})"
+	def __str__(self):
+		return f"{self.title} ({self.date_start.date() if self.date_start else 'TBD'})"
