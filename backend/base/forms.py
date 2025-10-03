5891f9e8f661f0c3c2caa5e281f9b95ee2d2874b
Yulian Tolkachov <56gymnasia@gmail.com>
2025-10-03 11:00:28 +0300

    feat:templates + Basic User Registration and Authentication

---
+++ backend/base/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        help_text="Required. Enter a valid email address."
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        # Add Bootstrap styling
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.utils import timezone
+from .models import Event
+from .forms import SignUpForm
 
 # Create your views here.
+
+def home(request):
+	events = Event.objects.all().order_by("-date_start")
+	return render(request, "index.html", {"events": events})
+
+def single(request, event_id):
+	event = Event.objects.get(pk=event_id)
+	if event and event.date_start:
+		time_remaining = event.date_start - timezone.now()
+		days = time_remaining.days
+		hours = (time_remaining.seconds // 3600)
+		minutes = (time_remaining.seconds % 3600) // 60
+		seconds = time_remaining.seconds % 60
+		data = {
+			'event': event,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		}
+	else:
+		data = {
+			'event': None,
+			'days': 0,
+			'hours': 0,
+			'minutes': 0,
+			'seconds': 0
+		}
+	return render(request, 'single.html', {'data': data})
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after signup
+            return redirect("home")
+    else:
+        form = SignUpForm()
+    return render(request, "signup.html", {"form": form})
